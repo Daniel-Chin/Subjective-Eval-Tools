8790a60524d826ae9ee7987e390984a78256052d
Daniel Chin <daniel.chin@nyu.edu>
2025-03-23 21:49:44 +0400

    first

---
+++ anonymize.py
@@ -0,0 +1,90 @@
+'''
+The anonymized names are salted hashes. Even if you lose the lookup table, as long as you keep the salt as well as the pre-anonymized filenames, everything can be recovered.  
+About the salt:  
+- I recommend using a short word that you can remember.  
+- Keep the salt file around.  
+- Cryptographically, as long as the survey respondents don't know the salt, they can't deanonymize.  
+- An additional benefit is you can re-run the script and the correspondence won't change. (e.g. No need to re-upload to SoundCloud)  
+
+This script assumes certain dir structure of the src folder. Modify code after the "# modify" tag if needed.  
+'''
+
+import os
+from os import path
+import sys
+import json
+import shutil
+import argparse
+import hashlib
+
+CODE_LENGTH = 4
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Anonymize audio files.')
+    parser.add_argument(
+        '--src', help='Input source folder of audio files', 
+        type=str, required=True, 
+    )
+    parser.add_argument(
+        '--salt', help='Input salt file, containing a raw string in ascii.', 
+        type=str, required=True, 
+    )
+    parser.add_argument(
+        '--dest', help='Output folder of anonymized files.', 
+        type=str, required=True, 
+    )
+    parser.add_argument(
+        '--lookup', help='Output lookup file.', 
+        type=str, required=True, 
+    )
+    args = parser.parse_args()
+    src: str = args.src
+    salt: str = args.salt
+    dest: str = args.dest
+    lookup: str = args.lookup
+    return src, salt, dest, lookup
+
+def askConfirm():
+    if input('y/n >').lower() != 'y':
+        print('aborted')
+        sys.exit(1)
+
+def main():
+    src, salt_filename, dest, lookup_filename = parseArgs()
+    if path.exists(dest):
+        print('Warning: destination folder already exists. Overwrite?')
+        askConfirm()
+        shutil.rmtree(dest)
+    os.makedirs(dest, exist_ok=False)
+    if path.isfile(lookup_filename):
+        print('Warning: anonymization lookup file already exists. Overwrite?')
+        print('This is a dangerous operation!!!')
+        askConfirm()
+    with open(salt_filename, 'r') as f:
+        salt = f.read()
+    lookup = []
+    used_ano_names = set()
+    # modify
+    # This treats the dir structure of src folder.
+    for task in os.listdir(src):
+        filenames = os.listdir(path.join(src, task))
+        filenames.sort()
+        for filename in filenames:
+            stem, ext = path.splitext(filename)
+            task_, song_name, model = stem.split('-')
+            assert task == task_
+            hash_code = hashlib.sha256(f'{filename}{salt}'.encode()).hexdigest()
+            anonymized = f's_{hash_code[:CODE_LENGTH]}{ext}'
+            if anonymized in used_ano_names:
+                raise ValueError(f'Hash collision! Increase `CODE_LENGTH` and try again.')
+            used_ano_names.add(anonymized)
+            shutil.copy(path.join(src, task, filename), path.join(dest, anonymized))
+            lookup.append((
+                anonymized, task, song_name, model, 
+            ))
+    with open(lookup_filename, 'w') as f:
+        json.dump(lookup, f, indent=2)
+    print('ok')
+
+if __name__ == '__main__':
+    main()
---
+++ soundcloud_auto_gui.py
@@ -0,0 +1,181 @@
+'''
+A tool for batch copying embed html code from soundcloud.  
+It controls your keyboard to do that.  
+You need the Vimium extension in your browser.  
+
+To copy:  
+1. Open your album (list of audios) page in soundcloud.  
+2. Press ` in your browser to start the automated GUI sequence.  
+2.1. Correction! You need to press Enter in the terminal instead. See comments in code.  
+3. Wait for the browser to return to the original state.  
+4. Finished. You can now paste.  
+
+To abort during automation: Press `  
+
+Additional notes:  
+- This setup is necessary because SoundCloud's embed html of a private song contains a secret token.  
+'''
+
+import typing as tp
+import os
+import json
+import threading
+import time
+
+import keyboard
+
+SKIP_TO: str | None = None  # anonymized filename
+
+SHORT_DELAY = 0.1
+WEBPAGE_LOAD_DELAY = 1
+
+def Samples():
+    # modify this function to your need.
+    with open('anonymization_lookup.json', 'r') as f:
+        lookup = json.load(f)
+    
+    for filename, task, song, model in lookup:
+        filename: str
+        task: str
+        song: str
+        model: str
+        yield filename, task, song, model
+
+def Pages():
+    # modify this function to your need.
+    samples = Samples()
+    current_page = None
+    acc = []
+    while True:
+        try:
+            filename, task, song, model = next(samples)
+        except StopIteration:
+            yield tuple(acc)
+            break
+        if current_page is None:
+            current_page = (task, song)
+            acc.append((filename, task, song, model))
+        else:
+            if current_page == (task, song):
+                acc.append((filename, task, song, model))
+            else:
+                yield tuple(acc)
+                acc = [(filename, task, song, model)]
+                current_page = (task, song)
+
+def unitTestPages():
+    a = [*Samples()]
+    b = []
+    for page in Pages():
+        b.extend(page)
+    assert a == b
+
+def SortedPages():
+    # place the prompt at the top.
+    for page in Pages():
+        sorted_page: tp.List[tp.Tuple[str, str, str, str]] = []
+        for filename, task, song, model in page:
+            if model == 'prompt':
+                sorted_page.insert(0, (filename, task, song, model))
+            else:
+                sorted_page.append((filename, task, song, model))
+        yield sorted_page
+
+def inspectSortedPages():
+    for page in SortedPages():
+        for filename, task, song, model in page:
+            print(filename, task, song, model)
+        input()
+
+def main():
+    print(__doc__)
+
+    do_abort = False
+    def abort():
+        nonlocal do_abort
+        do_abort = True
+    
+    is_during_automation = False
+    barrier = threading.Lock()
+    barrier.acquire()
+
+    # register the hotkey
+    def onHotKey():
+        time.sleep(0.1)
+        print(f'{is_during_automation = }')
+        if is_during_automation:
+            abort()
+        else:
+            barrier.release()
+
+    def sleep(n_sec: float | int):
+        time.sleep(n_sec)
+        if do_abort:
+            raise EOFError('aborted')
+    
+    skip_done = SKIP_TO is None
+    keyboard.add_hotkey('`', onHotKey)
+    try:
+        print('Awaiting input...')
+        for page in SortedPages():
+            print('new page:', page[0][1], page[0][2])
+            for filename, _, _, _ in page:
+                if not skip_done:
+                    assert SKIP_TO is not None
+                    if SKIP_TO in filename:
+                        skip_done = True
+                    else:
+                        print('skipping:', filename)
+                        continue
+                print('next up:', filename)
+
+                # hotkey isn't working on my machine... Switching to manual
+                # barrier.acquire()
+                input('Press enter to continue...')
+
+                is_during_automation = True
+
+                sleep(1)
+                keyboard.send('end')    # go to end of list
+                sleep(WEBPAGE_LOAD_DELAY)
+                keyboard.send('ctrl+f') # browser search bar
+                sleep(SHORT_DELAY)
+                stem, _ = os.path.splitext(filename)
+                keyboard.write(         # song title
+                    stem.split('_')[-1]  # https://github.com/boppreh/keyboard/issues/434
+                )
+                sleep(SHORT_DELAY)
+                keyboard.send('enter')  # focus on search hit
+                sleep(SHORT_DELAY)
+                keyboard.send('esc')    # exit search bar
+                sleep(SHORT_DELAY)
+                keyboard.send('enter')  # go to song page
+                sleep(WEBPAGE_LOAD_DELAY)
+                keyboard.send('page down')  # depends on your window height. I need this to get the "share" button in the view.  
+                sleep(0.5)
+                keyboard.write('f', delay=SHORT_DELAY)  # vimium
+                sleep(0.5)
+                # sleep(1)    # for debug
+                # select "share" button
+                keyboard.write('af', delay=SHORT_DELAY) # this code may change.
+                sleep(0.7)
+                keyboard.write('fc', delay=SHORT_DELAY) # vimium. select "embed"
+                sleep(0.5)
+                for _ in range(4):
+                    keyboard.send('tab')    # cycle to the code textbox
+                    sleep(SHORT_DELAY)
+                keyboard.send('ctrl+c')     # copy
+                sleep(SHORT_DELAY)
+                keyboard.send('alt+left')   # back to album page
+                sleep(0.3)
+                keyboard.send('f5')         # refresh
+
+                is_during_automation = False
+    finally:
+        keyboard.remove_all_hotkeys()
+    print('ok')
+
+if __name__ == '__main__':
+    unitTestPages()
+    # inspectSortedPages()
+    main()
